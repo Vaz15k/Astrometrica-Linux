df0758b23d0699a02b92a6b29ff0ee69be6ef419
Keven Eduardo <79882241+Vaz15k@users.noreply.github.com>
2024-04-12 13:40:10 -0400

    Atualização astrometrica.sh
    - Foi removido a função de atualização de versão ubuntu, ainda é altamente recomendado que você atualize o sistema...
    - Adicionado o pacote wget para ser instalado
    - Corrigido alguns erros de logica

--- astrometrica.sh
+++ astrometrica.sh
@@ -9,36 +9,22 @@ username=$(whoami)
 ## Pacotes do Ubuntu
 install_packages_ubuntu() {
     echo "Atualizando e instalando pacotes no Ubuntu..."
-    sudo apt-get update
-    sudo apt-get install -y gzip zip curl wine-stable wine-mono wine-gecko 
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install -y gzip curl wget wine-stable 
 }
 
 ## Pacotes do Arch Linux
 install_packages_arch() {
     echo "Atualizando e instalando pacotes no Arch Linux..."
     sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm gzip zip unzip curl wine wine-mono wine-gecko
+    sudo pacman -S --noconfirm gzip curl wget wine wine-mono wine-gecko || { echo "Verifique se repositorio multilib está ativo"; }
 }
 
 ## Pacotes do Fedora
 install_packages_fedora() {
     echo "Atualizando e instalando pacotes no Fedora..."
     sudo dnf update -y
-    sudo dnf install -y gzip zip curl wine
-    ## Fedora pode requerer repositórios adicionais para wine-mono e wine-gecko
-}
-
-## Função para atualizar Ubuntu para a versão mais recente
-upgrade_ubuntu() {
-    echo "Verificando a versão do Ubuntu..."
-    echo "Deseja verificar e aplicar a atualização do Ubuntu para a versão mais recente disponível? (s/n)"
-    read resposta
-
-    if [[ "${resposta,,}" == "s" ]]; then
-        echo "Preparando para atualizar o Ubuntu..."
-        sudo apt-get install -y update-manager-core
-        sudo do-release-upgrade
-    fi
+    sudo dnf install -y gzip wget curl wine
 }
 
 ## Instalador / Executor do Astrometrica
@@ -57,9 +43,9 @@ install_astrometrica() {
         fi
 
     else
-		clear
+	clear
         echo "Executando Astrometrica..."
-		cd "$HOME/.wine/drive_c/Astrometrica" || { echo "Falha ao acessar o diretório Astrometrica"; exit 1; }
+	cd "$HOME/.wine/drive_c/Astrometrica" || { echo "Falha ao acessar o diretório Astrometrica"; exit 1; }
         curl --insecure "https://minorplanetcenter.net/iau/MPCORB/MPCORB.DAT.gz" -o "MPCORB.DAT.gz"
         gzip -d -f MPCORB.DAT.gz
         mv MPCORB.DAT Catalogs/
@@ -72,16 +58,15 @@ if [ "$(id -u)" -eq 0 ]; then
 
     ## Determina o tipo de sistema e instala/atualiza os pacotes
     if grep -qi ubuntu /etc/os-release; then
-		clear
+	clear
         install_packages_ubuntu || { echo "Falha ao atualizar os pacotes do Ubuntu"; exit 1; }
-        upgrade_ubuntu || { echo "Falha ao atualizar o Ubuntu"; exit 1; }
 
     elif grep -qi arch /etc/os-release; then
-		clear
+	clear
         install_packages_arch || { echo "Falhar ao atualizar os pacotes do Arch Linux"; exit 1; }
 
     elif grep -qi fedora /etc/os-release; then
-		clear
+	clear
         install_packages_fedora || { echo "Falha ao atualizar o Fedora"; exit 1; }
     fi
 
